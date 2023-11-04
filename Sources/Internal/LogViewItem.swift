a46066797ef317e4bdbc304272e9c2b60c3e810a
Alexey Nenastev <a-nenastev@mail.ru>
2023-11-04 23:31:54 +0100

    fix.

--- Sources/Internal/LogViewItem.swift
+++ Sources/Internal/LogViewItem.swift
@@ -99,38 +99,3 @@ struct LogViewItem: View {
     }
   }
 }
-
-public extension View {
-  func frameSize() -> some View {
-    modifier(FrameSize())
-  }
-
-  var asAnyView: AnyView {
-    AnyView(self)
-  }
-}
-
-private struct FrameSize: ViewModifier {
-  static let color: Color = .blue
-
-  func body(content: Content) -> some View {
-    content
-      .overlay(GeometryReader(content: overlay(for:)))
-  }
-
-  func overlay(for geometry: GeometryProxy) -> some View {
-    ZStack(
-      alignment: Alignment(horizontal: .trailing, vertical: .top)
-    ) {
-      Rectangle()
-        .strokeBorder(
-          style: StrokeStyle(lineWidth: 1, dash: [5])
-        )
-        .foregroundColor(FrameSize.color)
-      Text("\(Int(geometry.size.width))x\(Int(geometry.size.height))")
-        .font(.caption2)
-        .foregroundColor(FrameSize.color)
-        .padding(2)
-    }
-  }
-}
